bbf22e3a26be7c09636834b162ddbb783a3b7f35
w0443465 <w0443465@nscc.ca>
2021-11-19 20:52:29 -0400

    unity proj 1

---
+++ KindasAndroidApp1/Assets/Scripts/Controls.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float movespeed;
+    public bool moveright;
+    public bool moveleft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //keyboard controls
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);
+        }
+
+        //touch controls
+        if (moveright)
+         {
+         rb.velocity = new Vector2(movespeed, rb.velocity.y);
+         }
+         if (moveleft)
+         {
+         rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+         }
+    }
+}
